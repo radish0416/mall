fc4509a9621623b02c07e9ced7cadc5056c90b4f
lixianjiang <lixianjiang@pekon.com>
2021-02-08 13:30:31 +0800

    连接数据库

--- mall-admin/src/main/java/com/macro/mall/service/impl/UmsAdminServiceImpl.java
+++ mall-admin/src/main/java/com/macro/mall/service/impl/UmsAdminServiceImpl.java
@@ -123,7 +123,9 @@ public class UmsAdminServiceImpl implements UmsAdminService {
      */
     private void insertLoginLog(String username) {
         UmsAdmin admin = getAdminByUsername(username);
-        if(admin==null) return;
+        if(admin==null) {
+            return;
+        }
         UmsAdminLoginLog loginLog = new UmsAdminLoginLog();
         loginLog.setAdminId(admin.getId());
         loginLog.setCreateTime(new Date());
